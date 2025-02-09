844a45c38a4a5a41f81997eb5975a82c94ddd4b1
MohanAskani <ma2421@rutgers.edu>
2025-02-08 20:53:30 -0500

    Initial commit

--- best_time_to_buy&sell_stock.py
+++ best_time_to_buy&sell_stock.py
@@ -1,1 +1,19 @@
-Hi
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        minPrice = prices[0]
+        profit = 0
+
+        for price in prices[1:]:
+            if price < minPrice:
+                minPrice = price
+            elif profit < (price - minPrice):
+                profit = price - minPrice
+        return profit
+
+def main():
+    func_ = Solution()
+    a = list(map(int, input("Enter list of stock prices").split()))
+    print(func_.maxProfit(a))
+
+if __name__ == "__main__":
+    main()
